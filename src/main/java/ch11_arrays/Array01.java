e82cee3ca084e5afc7c91155e3d40fb2cdddead6
enfia <enfiaxd@gmail.com>
2025-12-31 17:12:27 +0900

    202512310512

--- src/main/java/ch11_arrays/Array01.java
+++ src/main/java/ch11_arrays/Array01.java
@@ -2,6 +2,32 @@ package ch11_arrays;
 
 public class Array01 {
     public static void main(String[] args) {
-
+        int[] arr04 = new int[40];
+        for(int i= 0; i < arr04.length; i++){
+            arr04[i] = 41 + i;
+            System.out.print(arr04[i]+ " ");
+        }
+//        String[] arr05 = new String[5];
+//        arr05[0] = "안";
+//        arr05[1] = "녕";
+//        arr05[2] = "하";
+//        arr05[3] = "세";
+//        arr05[4] = "요";
+        String[] arr05 = {"안", "녕", "하", "세", "요"};
+        for(int i=0; i < arr05.length; i++){
+            System.out.print(arr05[i]);
+        }
+//        int[] arr03 = new int[10];
+//        System.out.println(arr03[0]);
+//        arr03[0] = 1000;
+//        System.out.println(arr03[0]);
+//        int[] arr01 = {1,2,3,4,5};
+//
+//        for(int i= 0; i < arr01.length; i++){
+//            System.out.print(arr01[i] + " ");
+//        }
+//        for (int i = 0; i < 5; i++){
+//            System.out.print(arr01[i]+" ");
+//        }
     }
 }
---
+++ src/main/java/ch11_arrays/Array02.java
@@ -0,0 +1,26 @@
+package ch11_arrays;
+
+import java.util.Scanner;
+
+public class Array02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("몇 명의 학생을 등록하시겠습니까? >>> ");
+        int count = sc.nextInt();
+        sc.nextLine();
+        String[] humans = new String[count];
+        for(int i = 0; i< humans.length; i++){
+            System.out.println((i + 1)+"번 학생의 이름을 입력해주세요 >>>");
+            humans[i] = sc.nextLine();
+        }
+        int[] score = new int[count];
+        int scoreSum = 0;
+        for(int i = 0; i<humans.length; i++){
+           System.out.println(humans[i] +" 학생의 점수를 입력해주세요 >>>");
+           score[i] = sc.nextInt();
+           sc.nextLine();
+           scoreSum += score[i];
+        }
+        System.out.println( "학생들 점수의 평균은 " + scoreSum/count);
+    }
+}
