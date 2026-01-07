0852101639bde8afc16d39d9eac6fc7c690f45a7
enfia <enfiaxd@gmail.com>
2026-01-07 17:26:01 +0900

    202601071725

--- src/main/java/ch13_abstraction/interfaces/Button.java
+++ src/main/java/ch13_abstraction/interfaces/Button.java
@@ -1,11 +1,11 @@
 package ch13_abstraction.interfaces;
 
-
-
+// 부모 클래스임
 public abstract class Button implements Press, Up, Down {
     @Override
     public void onDown() {}
 
+    //얘는 필수로 만들어야함
     @Override
     public abstract void onPressed();
 
--- src/main/java/ch13_abstraction/interfaces/Down.java
+++ src/main/java/ch13_abstraction/interfaces/Down.java
@@ -3,6 +3,7 @@ package ch13_abstraction.interfaces;
 /*
 요약
 인터페이스인데
+필수로 선언 해야 되는건 onDown임
  */
 // 인터페이스 Down임
 public interface Down {
